60f3609907686293c46201f4bb01d8fc237e98a4
Sen2017 <casintahan47at@gmail.com>
2017-03-06 08:43:02 -1000

    push 3.6.17

--- app.js
+++ app.js
@@ -4,23 +4,31 @@
 */
 function accessObjectInArray(arr){
 	//Print the parameter being passed through
-
+	//console.log(arr);
 	//How can we tell what type of data this is?
 	//By it's notation, the top level is bracket notation, therefore the parameter is an array
 
 	//What methods can we use for this item?
 	//Array methods
-
+	//console.log(arr.length);
 	//Access and print the first object in the parameter 
-
+	//console.log(arr[0]);
 	//Access and print the third object in the parameter
-
+	//console.log(arr[2]);
 	//Iterate through the array to print all the objects in the parameter
+	for(var i = 0; i < arr.length; i++){
+		//console.log("app.js line 20 ");
+		//console.log(arr[i]);
+
+			for (var key in arr){
+				//console.log(key);
+				//console.log(arr[key]);
+			}
+	}
 
 		//Can you concatanete a string to an object?
-
+		 //NO 
 		//Iterate through the key:value pairings and print all the keys and their values
-
 	//Clean up your code by creating variable names and setting their values
 }
 
@@ -48,15 +56,28 @@ console.log("\n*** End ***\n")
 */
 function accessArrayInObject(obj){
 	//Is the parameter an array or an object?
-
+	
 	//Print the paramter being passed through the function
-
+	console.log(obj);
 	//Iterate through the object
-
+	for(var key in obj){
+		console.log("\nKey");
+		console.log(key);
+		console.log("\nValue");
+		console.log(obj[key]);
 	//Create a conditional to check if the key is "cars"
+		if (key === "cars"){
+		console.log("\ninside conditional for cars");
+		console.log(obj[key]);
+	 	}
+	
+		for (var carValue in obj[key]){
+			console.log("\n Value for cars");
+			console.log(obj[key][carValue]);
+		}
+	}
 
 	//Iterate through and print the values in the key "cars"
-
 	//Iterate through and print the key value pairs for each object in the array "toppings"
 
 	//Clean up your code by creating variable names and setting their values
